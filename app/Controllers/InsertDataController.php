536d4e3562357ddac3d995a6f58b8ce383592d0e
unknown <senac@uept46-13597-a.aluno.pr.senac.br>
2022-09-29 22:41:39 -0300

    quase pronto o projeto post

--- app/Controllers/InsertDataController.php
+++ app/Controllers/InsertDataController.php
@@ -3,52 +3,66 @@
 namespace App\Controllers;
 
 use App\FrameworkTools\Abstracts\Controllers\AbstractControllers;
-use App\FrameworkTools\Database\DatabaseConnection;
 
 class InsertDataController extends AbstractControllers{
 
-    public function exec() {
-        $pdo = DatabaseConnection::start()->getPDO();
-        $params = $this->processServerElements->getInputJSONData();
-        $response = ['success'=> true];
-        $attrName;
-
-        try {
-            
-            if (!$params['name']) {
-                $attrName = 'name';
-                throw new \Exception('the name has to be send in the request');
-            }   
+    private $params;
+    private $attrName;
     
-            if (!$params['last_name']) {
-                $attrName = 'last_name';
-                throw new \Exception('the last_name has to be send in the request');
-            }
+    public function exec() {
+        try {
+            $response = ['success'=> true];
 
-            if (!$params['age']) {
-                $attrName = 'age';
-                throw new \Exception('the age has to be send in the request');
-            }
+            $this->params = $this->processServerElements->getInputJSONData();
+    
+            $this->verificationInputVar();
     
             $query = "INSERT INTO user (name,last_name,age) VALUES (:name,:last_name,:age)";
             
-            $statement = $pdo->prepare($query);
-     
+            $statement = $this->pdo->prepare($query);     
             $statement->execute([
-                ':name' => $params["name"],
-                ':last_name' => $params["lastName"],
-                ':age' => $params["age"]
+                ':name' => $this->params["name"],
+                ':last_name' => $this->params["last_name"],
+                ':age' => $this->params["age"]
             ]);
 
         } catch (\Exception $e) {
             $response = [
                 'success' => false,
                 'message' => $e->getMessage(),
-                'missingAttribute' => $attrName
+                'missingAttribute' => $this->attrName
             ];
         }
 
         view($response);
+    }
+
+
+    private function verificationInputVar() {
+        if (!$this->params['name']) {
+            $this->attrName = 'name';
+            throw new \Exception('the name has to be send in the request');
+        }   
+
+        if (!$this->params['last_name']) {
+            $this->attrName = 'last_name';
+            throw new \Exception('the last_name has to be send in the request');
+        }
+
+        if (!$this->params['age']) {
+            $this->attrName = 'age';
+            throw new \Exception('the age has to be send in the request');
+        }
+
+        if ($this->params['age'] < 0) {
+            $this->attrName = 'age';
+            throw new \Exception('the age has to be more than zero');
+        }
+
+        if ($this->params['age'] > 200) {
+            $this->attrName = 'age';
+            throw new \Exception('the age has to be less than two hundred');
+        }        
     }
 
 }
--- app/FrameworkTools/Abstracts/Controllers/AbstractControllers.php
+++ app/FrameworkTools/Abstracts/Controllers/AbstractControllers.php
@@ -3,16 +3,19 @@
 namespace App\FrameworkTools\Abstracts\Controllers;
 
 use App\FrameworkTools\ProcessServerElements;
+use App\FrameworkTools\Database\DatabaseConnection;
 
 abstract class AbstractControllers {
     
     protected $processServerElements;
+    protected $pdo;
 
     public function __construct() {
         $typeOfAPI = env('TYPE_API');
         header("Content-Type: application/$typeOfAPI");
 
         $this->processServerElements = ProcessServerElements::start();
+        $this->pdo = DatabaseConnection::start()->getPDO();
     }
 
 }
